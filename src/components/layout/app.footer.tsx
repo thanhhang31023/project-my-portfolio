d7069b3454c11eaea5ad3d85ea20b214512bfb88
thanhhang31023 <thanhhang31023@gmail.com>
2025-03-21 12:24:25 +0900

    create footer

--- src/components/layout/app.footer.tsx
+++ src/components/layout/app.footer.tsx
@@ -1,10 +1,17 @@
+import { useTranslation } from "react-i18next";
 
 const AppFooter = () => {
+        const { t } = useTranslation();
     return (
-        <div className="text-center my-3">
-            Copyright © {new Date().getFullYear()} HANG DEV FRONT END. 
+        <div className="text-center my-3 text-sm mt-2">
+            <p className="slogan text-2xl md:text-xl sm:text-lg text-center px-4">
+            {t("appFooter.slogan")}
+            </p>
+            <p className="text-gray-400 text-sm mt-2">
+                Copyright © {new Date().getFullYear()} Hang dev front end.
+            </p>
         </div>
-    )
+    );
 }
 
 export default AppFooter;
